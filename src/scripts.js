281c4a2e63044e743d4ecddf569933a7055da4d3
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-09-21 18:32:23 -0600

    Add getAllData fetch function

--- src/scripts.js
+++ src/scripts.js
@@ -3,9 +3,24 @@
 // Do not delete or rename this file ********
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
-
+import './apiCalls'
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+export let customersData;
+export let bookingsData;
+export let roomsData;
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+const getAllData = () => {
+  Promise.all([getCustomers(), getBookings(), getRooms()])
+  .then(data => {
+    customersData = data[0].customers;
+    bookingsData = data[1].bookings;
+    roomsData = data[2].rooms;
+  })
+}
+
+getAllData()
+
