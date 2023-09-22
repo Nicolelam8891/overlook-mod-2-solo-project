e3d1561341dab3fcae6cacafde3f236167cd1c25
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-09-22 14:43:14 -0600

    Add bookings calculation

--- src/calculations.js
+++ src/calculations.js
@@ -1,5 +1,5 @@
-// export const calculateBookingsSum = (bookingsData) => {
-//   const bookingsSum = bookingsData.reduce((acc, booking) => {
-//     acc = acc + booking.room.costPerNight
-//   }, 0).toFixed(2);
-// }
+export const calculateBookingsSum = (bookingsData) => {
+  const bookingsSum = bookingsData.reduce((acc, booking) => {
+    acc = acc + booking.room.costPerNight
+  }, 0).toFixed(2);
+}
--- test/calculations-test.js
+++ test/calculations-test.js
@@ -1,16 +1,16 @@
-// import { calculateBookingsSum } from "../src/calculations";
+import { calculateBookingsSum } from "../src/calculations";
 
-// import chai from "chai";
-// const expect = chai.expect;
+import chai from "chai";
+const expect = chai.expect;
 
-// describe("calculate customer bookings", function () {
-//   it("should be a function", function () {
-//     expect(calculateBookingsSum).to.be.a("function");
-//   });
+describe("calculate customer bookings", function () {
+  it("should be a function", function () {
+    expect(calculateBookingsSum).to.be.a("function");
+  });
 
-//   it("should calculate the total spent on all bookings for the customer", function () {
-//     let result = calculateBookingsSum()
-//     expect(calculateBookingsSum).to.be.a("function");
-//   });
-// });
+  it("should calculate the total spent on all bookings for the customer", function () {
+    let result = calculateBookingsSum()
+    expect(calculateBookingsSum).to.be.a("function");
+  });
+});
 
