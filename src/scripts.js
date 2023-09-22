13eec8edf5a5f6576e7005a2421168ca8289e343
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-09-21 19:24:43 -0600

    Add working getFetch function

--- src/scripts.js
+++ src/scripts.js
@@ -3,7 +3,7 @@
 // Do not delete or rename this file ********
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
-import './apiCalls'
+import { getCustomers, getBookings, getRooms } from './apiCalls'
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
@@ -17,8 +17,11 @@ const getAllData = () => {
   Promise.all([getCustomers(), getBookings(), getRooms()])
   .then(data => {
     customersData = data[0].customers;
+    // console.log("customersData:=====", customersData);
     bookingsData = data[1].bookings;
+    // console.log("bookingsData:=====", bookingsData);
     roomsData = data[2].rooms;
+    // console.log("roomsData:=====", roomsData);
   })
 }
 
