51b536190e0c69c98e8ad2bf53481eb304e9ee2f
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-09-24 11:13:45 -0600

    Add checkValidCustomerLogin function and fixed failing test

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -6,7 +6,7 @@ import { getAllCustomerBookings } from "./customer-bookings"
 const dashboardPage = document.querySelector(".dashboard-page");
 const loginPage = document.querySelector(".login-page");
 
-export const loadHomePage = () => {
+export const loadDashboardPage = () => {
   dashboardPage.classList.remove("hidden")
   loginPage.classList.add("hidden")
 }
--- src/login.js
+++ src/login.js
@@ -1,24 +1,25 @@
 //test for login information 
-import { loadHomePage } from "./domUpdates"
-
-
-
 export let handleLogin = (username, password) => {
-  const userId = username.slice(8)
-
   if (username === "" || password === "") {
     // console.log("Please enter both your username and password.")
     return "Please enter both your username and password."
-  }
+  } 
 
   if (username.includes("customer") && password === "overlook2021") {
-    loadHomePage()
-    
-    return parseInt(userId)
-    // return parseInt(userId); change string into a number
+    return true
   } else {
     // console.log("Invalid credentials, please try again!")
     return "Invalid credentials, please try again!";
+  }
+}
+
+//checkValidCustomerLogin function gives you an id number
+export const checkValidCustomerLogin = (username) => {
+  const userId = username.slice(8) //take out "customer" but keeps the number, which will give me the id number "ex) 50"
+  if (userId <= 50) { //hardcoded here. Another way of doing this is maping the id, which will give me an array of id numbers, then use .includes to see if the id number is there. If it is there, then the login is successful. If the id is not there, login is not successful. 
+    return parseInt(userId)  // return parseInt(userId); change string into a number
+  } else {
+    return `Sorry, this is not a valid ID number, please try again.`
   }
 }
 
